03710379162af5bb770fa0192b09a1c85ee63a46
Luiza Schmidt Mescolotto <lsmescolotto@gmail.com>
2024-01-16 20:18:19 -0300

    feat: stopwatch

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -5,113 +6,96 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useState} from "react";
+import {Image, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App(): React.JSX.Element {
+  const [stopwatchValue, setStopwatchValue] = useState(0);
+  const [stopwatch, setStopwatch] = useState<any>(null);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const handleStartAndStopCount = () => {
+    if (stopwatch === null) {
+      setStopwatch(
+        setInterval(() => {
+          setStopwatchValue((state) => state + 0.1);
+        }, 100),
+      );
+    } else {
+      clearInterval(stopwatch);
+      setStopwatch(null);
+    }
+  };
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const handleResetCount = () => {
+    setStopwatchValue(0);
+    setStopwatch(null);
+  };
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <Image style={styles.img} source={require("./src/timer.png")} />
+      <View style={styles.stopwatchBackground}>
+        <Text style={styles.stopwatchCount}>{stopwatchValue.toFixed(1)}</Text>
+      </View>
+
+      <View style={styles.buttonArea}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleStartAndStopCount}>
+          <Text style={styles.buttonText}>
+            {stopwatch === null ? "Start" : "Stop"}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleResetCount}>
+          <Text style={styles.buttonText}>Reset</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#00aeef",
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  img: {
+    width: 340,
+    height: 340,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  stopwatchBackground: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#00aeef",
+    width: 150,
+    height: 150,
+    borderRadius: 100,
+    marginTop: -220,
   },
-  highlight: {
-    fontWeight: '700',
+  stopwatchCount: {
+    fontSize: 56,
+
+    color: "#000",
+  },
+  buttonArea: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 16,
+    marginTop: 120,
+  },
+  button: {
+    borderRadius: 5,
+    height: 50,
+    width: 80,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontWeight: "700",
+    fontSize: 25,
   },
 });
 
